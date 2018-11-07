731507055664319826a4bcf1542641430b9e2ec8
Harish Doraiswamy <harishd@nyu.edu>
2018-11-07 10:20:04 -0500

    updating scripts: adding inmemory attribute scaling

---
+++ experiments/run-scripts/raster-attributes-inmem.ps1
@@ -0,0 +1,28 @@
+param
+(
+	[Parameter(Mandatory)]
+	[string]$ProgramDir,
+	[Parameter(Mandatory)]
+	[string]$DataFolder,
+	[Parameter(Mandatory)]
+	[string]$OutputFolder
+)
+
+$exe = $ProgramDir + "\RasterJoin.exe"
+$index = $DataFolder + "\taxi\taxi_full_index"
+$polyList = $DataFolder + "\polys\nyc_polys.txt"
+
+$scalabilityFolder = $OutputFolder + "\scalability"
+if(!(Test-Path -Path $scalabilityFolder )){
+    New-Item -ItemType directory -Path $scalabilityFolder
+}
+
+$attribs = 0,1,2,3
+ForEach($nAttrib in $attribs) {
+	$opFile = $scalabilityFolder + "\taxi-mem-attrib.txt"
+	$arguments = "--nIter", 6, "--joinType", "raster", "--accuracy", 10, "--backendIndexName", "$index", "--locAttrib", 1, "--polygonList", "$polyList", "--polygonDataset", "neigh", "--startTime", 1230768000, "--endTime", 1246223000, "--nAttrib", $nAttrib, "--outputTime", "$opFile", "--inmem"
+	Write-Host("executing $exe $arguments")
+	& "$exe" $arguments
+}
+
+
